8a51dcfb5e0b8f2a6d9ca0cc7d638f2877562304
Pedro Damassceno <pedro.damasceno@hotmail.com>
2020-04-09 12:21:44 -0300

    Finalizado

--- copyH2.bat
+++ copyH2.bat
@@ -7,38 +7,66 @@ set SUBFILENAME=%CUR_YYYY%-%CUR_MM%-%CUR_DD%
 
 
 ::Caso tenham feito o upload do arquivo no FTP, o programa segue, caso contrario encerra
-IF EXIST "FILE MONITORED PATH" (
-	ECHO file 'FILE MONITORED PATH' exists
+IF EXIST "C:\FTP_ATIVOS\H2\uracache1.h2.db" (
+	ECHO file 'C:\FTP_ATIVOS\H2\uracache1.h2.db' exists
 	
 	
 :: CVP B -----------------------------------------------------------------------------------
 	::Pausa o servico VXML do CVP B
 	echo Inicio: %DATE% %TIME% > LogB%SUBFILENAME%.txt
-	SC \\SERVERNAME stop "VXMLServer"
-	SC \\SERVERNAME query "VXMLServer" >> LogB%SUBFILENAME%.txt
+	SC \\atvvmcccvpvxm1b stop "VXMLServer"
+	SC \\atvvmcccvpvxm1b query "VXMLServer" >> LogB%SUBFILENAME%.txt
 	timeout /t 120
-	SC \\SERVERNAME query "VXMLServer" >> LogB%SUBFILENAME%.txt
+	SC \\atvvmcccvpvxm1b query "VXMLServer" >> LogB%SUBFILENAME%.txt
 
 	::Renomeia o arquivo H2 da pasta do CVP B (H:\) e move para o diretorio OLD 
 	echo Renomeia para OLD 
-	ren 'FILE PATH + FILE NAME' 'FILE PATH + FILE NAME'OLD%SUBFILENAME%
+	ren H:\uracache1.h2.db uracache1OLD%SUBFILENAME%.h2.db
 	echo Move Para pasta OLD
-	move 'FILE PATH + FILE NAME'OLD%SUBFILENAME% 'PATH'
+	move H:\uracache1OLD%SUBFILENAME%.h2.db H:\OLD
 	echo Arquivo movido para pasta OLD
 	
 	::Copia o arquivo do FTP para o CVP
 	echo INICIO Copia FTP para CVPA %DATE% %TIME% >> LogB%SUBFILENAME%.txt
-	copy FILE MONITORED PATH_FROM PATH_TO
-	echo FIM Copia FTP para CVPA %DATE% %TIME% >> LogB%SUBFILENAME%.txt
+	copy C:\FTP_ATIVOS\H2\uracache1.h2.db H:\
+	echo FIM Copia FTP para CVPB %DATE% %TIME% >> LogB%SUBFILENAME%.txt
 	timeout /t 5
 	::Starta o Servico VXML do CVP
-	SC \\SERVERNAME start "VXMLServer"
-	SC \\SERVERNAME query "VXMLServer" >> LogB%SUBFILENAME%.txt
+	SC \\atvvmcccvpvxm1b start "VXMLServer"
+	SC \\atvvmcccvpvxm1b query "VXMLServer" >> LogB%SUBFILENAME%.txt
 	
 	echo Fim: %DATE% %TIME% >> LogB%SUBFILENAME%.txt
+
+
+
+:: CVP A -----------------------------------------------------------------------------------
+	::Pausa o servico VXML do CVP A
+	echo Inicio: %DATE% %TIME% > LogA%SUBFILENAME%.txt
+	SC \\atvvmcccvpvxm1a stop "VXMLServer"
+	SC \\atvvmcccvpvxm1a query "VXMLServer" >> LogA%SUBFILENAME%.txt
+	timeout /t 120
+	SC \\atvvmcccvpvxm1a query "VXMLServer" >> LogA%SUBFILENAME%.txt
+
+	::Renomeia o arquivo H2 da pasta do CVP B (G:\) e move para o diretorio OLD 
+	echo Renomeia para OLD 
+	ren G:\uracache1.h2.db uracache1OLD%SUBFILENAME%.h2.db
+	echo Move Para pasta OLD
+	move G:\uracache1OLD%SUBFILENAME%.h2.db G:\OLD
+	echo Arquivo movido para pasta OLD
+	
+	::Copia o arquivo do FTP para o CVP
+	echo INICIO Copia FTP para CVPA %DATE% %TIME% >> LogA%SUBFILENAME%.txt
+	copy C:\FTP_ATIVOS\H2\uracache1.h2.db G:\
+	echo FIM Copia FTP para CVPA %DATE% %TIME% >> LogA%SUBFILENAME%.txt
+	timeout /t 5
+	::Starta o Servico VXML do CVP
+	SC \\atvvmcccvpvxm1a start "VXMLServer"
+	SC \\atvvmcccvpvxm1a query "VXMLServer" >> LogA%SUBFILENAME%.txt
+	echo Fim: %DATE% %TIME% >> LogA%SUBFILENAME%.txt
+
 	
 ) ELSE (
-  ECHO file 'FILE MONITORED PATH' does NOT exist
+  ECHO file 'C:\FTP_ATIVOS\H2\uracache1.h2.db' does NOT exist
   ECHO FIM DO PROGRAMA
   echo Arquivo nao existe na pasta as: %DATE% %TIME% >> LogErro%SUBFILENAME%.txt
 )
